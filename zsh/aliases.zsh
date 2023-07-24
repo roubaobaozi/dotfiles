75ea3f27e333b160d31023539dd7c40379127180
baobaozi <work@mingteo.com>
2023-07-25 09:32:52 +1000

    update vim settings, and update fdock

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -136,7 +136,7 @@ ss() {
 alias arti='echo "AD Username: " && read AD_LOGIN && echo "AD Password: " && read -s AD_PASSWORD && curl -u "$AD_LOGIN:$AD_PASSWORD" https://artifactory.foxsports.com.au/api/npm/auth | sed -n "1p" | sed -e "s,_auth = ,,g" | read authstr && printf "{\n  \"auths\" : {\n    \"https://artifactory.foxsports.com.au:5003\" : {\n      \"auth\" : \"" >! ~/.docker/config.json && printf "$authstr" >> ~/.docker/config.json && printf "\"\n    },\n    \"artifactory.foxsports.com.au:5001\" : {\n      \"auth\" : \"" >> ~/.docker/config.json && printf "$authstr" >> ~/.docker/config.json && printf "\"\n    },\n    \"https://artifactory.foxsports.com.au:5001\" : {\n      \"auth\" : \"" >> ~/.docker/config.json && printf "$authstr" >> ~/.docker/config.json && printf "\"\n    },\n    \"artifactory.foxsports.com.au:5003\" : {\n      \"auth\" : \"" >> ~/.docker/config.json && printf "$authstr" >> ~/.docker/config.json && printf "\"\n    }\n  }\n}" >> ~/.docker/config.json && unset -v authstr && unset -v AD_LOGIN AD_PASSWORD authstr'
 
 # FUCKING DOCKER
-alias fuckdock='echo "You’ve come to the right place.\n\ndocker login https://artifactory.foxsports.com.au:5001\n    && docker login docker.pkg.github.com\n" && docker login https://artifactory.foxsports.com.au:5001 && docker login docker.pkg.github.com'
+alias fuckdock='echo "You’ve come to the right place.\n\ndocker login docker.pkg.github.com\n" && docker login docker.pkg.github.com'
 alias dockfuck='echo "You haven’t come to the right place, but fuck it let’s dock fuck anyway.\n" && fuckdock'
 
 alias sydtime='sudo systemsetup -settimezone Australia/Sydney'
