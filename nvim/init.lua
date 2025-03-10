943d32e783b1a0a2cfc7a98c375413944cb38d4e
baobaozi <work@mingteo.com>
2025-03-11 09:45:34 +1100

    Getting lazy working, so copilot can work, because emmet-vim no longer works in styled-components anyway (since nvim 0.10.x I think), so there's no reason really to stay on vim-plug now ..

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,30 @@
 -- editorConfig is enabled by default in neovim: https://neovim.io/doc/user/editorconfig.html
 -- To install all the nerd fonts:
 -- brew tap homebrew/cask-fonts && brew search '/font-.*-nerd-font/' | awk '{ print $1 }' | xargs -I{} brew install --cask {} || true
-require('plugins')
+
+vim.g.mapleader = ' ' -- Make sure to set `mapleader` before lazy so your mappings are correct, except not using lazy because it breaks emmet
+vim.g.maplocalleader = ' '
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
+
+-- require('plugins')
 require('options')
 require('custom_keymaps')
 
